2db3d96fcb828f08e863eb08e692023ab81dd06c
supermario6409 <43834781+supermario6409@users.noreply.github.com>
2022-02-02 20:36:07 -0500

    Rewrite of 2022
    A complete rewrite of the code that has been done on the github, Has been shown to drive, changes include: a rewrite of how the talonSRX motor controllers are defined, how the controller works, and how the basic drive train logic works, adapted to a command base project rather than a timed project.

--- src/main/java/frc/robot/BoomCannon/IntakeConveyorSubsystem.java
+++
@@ -1,5 +0,0 @@
-package frc.robot.BoomCannon;
-
-public class IntakeConveyorSubsystem {
-
-}
--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -3,45 +3,25 @@ package frc.robot;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
 public class RobotMap {
-    // Drivetrain safety toggles
+    /// Drive Train Safety Toggles ///
+
     public static boolean driveTrainSafety = true;
-    public static double driveSafetySpeedMod = 0.1;
+    public static double speedMod = .1;
     public static boolean driveSlowMod = false;
 
-    // conveyor safety
-    // arbitrary values
-    public static double conveyorSafetyMod = 0.1;
-    public static double placeholderConveyorSafetyMod = 0.1;
-
-    // shooter safety
-    // note: arbitrary values
-    public static final double lowPortShooterSpeedMod = 0.3;
-    public static final double highPortShooterSpeedMod = 0.6;
-    public static final double placeholderShooterSpeedMod = 0.1;
-
-    // debug toggles
+    /// Debug Toggles ///
     public static final boolean debug = false;
     public static final boolean driveDebug = true;
 
-    // drivetrain deadzone value
-    public static double deadzone = 0.25;
+    /// Drive Train Deadzone Value ///
+    public static double deadzone = 0.15;
 
-    // control mode (required for VICTORSPX.set() functions)
-    public ControlMode controlMode = ControlMode.PercentOutput;
+    /// Control Mode (Required for VICTORSPX.set() functions) ///
+    public static ControlMode controlMode = ControlMode.PercentOutput;
 
-    // Motor Ports (CAN IDs)
-    // note arbitrary values
-    // started at 1, moved down to 8
-    public static int leftMotorLeader = 4; // left side
-    public static int leftMotorFollower = 2;
-    public static int rightMotorLeader = 1; // right side
-    public static int rightMotorFollower = 3;
-    // special functions motors
-    // shooter motors
-    public static int shooterMotorTop = 5;
-    public static int shooterMotorBottom = 6;
-    // intake motor
-    public static int intakeConveyorMotor = 7;
-    // pulley motor
-    public static int pulleyMotor = 8;
+    /// Motor Ports (CAN BUS) ///
+    public static final int leftMotorLeader = 4;
+    public static final int leftMotorFollower = 2;
+    public static final int rightMotorLeader = 1;
+    public static final int rightMotorFollower = 3;
 }
